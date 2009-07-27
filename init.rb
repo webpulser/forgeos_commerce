121021113256affb9f7e9dec4ecb3812d6e745ae
clepagnot <clepagnot@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-07-27 13:21:19 +0000

    - remove common locales

--- init.rb
+++ init.rb
@@ -1,14 +1,4 @@
-
-locale_path = File.join(directory, 'config/locales')
-if File.exists?(locale_path)
-  locale_files = Dir[File.join(locale_path, '*.{rb,yml}')]
-  unless locale_files.blank?
-    first_app_element = 
-      I18n.load_path.select{ |e| e =~ /^#{ RAILS_ROOT }/ }.reject{ |e| e =~ /^#{ RAILS_ROOT }\/vendor\/plugins/ }.first
-    app_index = I18n.load_path.index(first_app_element) || - 1
-    I18n.load_path.insert(app_index, *locale_files)
-  end
-end
+I18n.load_path += Dir[Rails.root.join('vendor', 'plugins', 'rails_commerce', 'config', 'locales', '*.{rb,yml}')]
 config.gem "acts_as_ferret", :lib => "acts_as_ferret"
 config.gem "rubyist-aasm", :source => "http://gems.github.com", :lib => 'aasm'
 
