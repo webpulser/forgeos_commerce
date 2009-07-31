8283e2deda62a9d6e2024ed2464a7d943e9fb939
rendelin <rendelin@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-07-31 08:26:47 +0000

    configuration of spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + "/../../../../config/environme
 require 'spec'
 require 'spec/rails'
 
+Dir[File.dirname(__FILE__) + "/views/shared_examples/**/*_spec.rb"].each {|file| require(file)}
+Dir[File.dirname(__FILE__) + "/spec_helpers/**/*.rb"].each {|file| require(file) }
+
 Spec::Runner.configure do |config|
   config.fixture_path = "#{File.dirname(__FILE__)}/../spec/fixtures"
+  
+  config.include ControllerHelpers::Uploader, :type => :controller
+  config.include LoginHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
 end
