4c1a725e2b2048469dd96757ff36f20816bdde99
clepagnot <clepagnot@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-08-11 16:11:02 +0000

    - remove unecessary entries in admin menu

--- lib/rails_commerce.rb
+++ lib/rails_commerce.rb
@@ -52,9 +52,6 @@ Forgeos::Menu << { :title => ['wishlist',{ :count => 1}],
   :i18n => true 
 }
 
-Forgeos::AdminMenu << {:title => 'special offer', :url => { :controller => 'admin/special_offers'}}
-Forgeos::AdminMenu << {:title => 'shipping rule', :url => { :controller => 'admin/shipping_rules'}}
-
 # Set attachable media types
 Forgeos::AttachableTypes << 'Product'
 Forgeos::AttachableTypes << 'ShippingMethod'
