08d159cc51b1503cd28bff885c6f63ae1be221b3
fcogez <fcogez@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-08-17 12:37:52 +0000

    Remove acts_as_ferret

--- init.rb
+++ init.rb
@@ -1,5 +1,4 @@
 I18n.load_path += Dir[Rails.root.join('vendor', 'plugins', 'rails_commerce', 'config', 'locales', '*.{rb,yml}')]
-config.gem "acts_as_ferret", :lib => "acts_as_ferret"
 config.gem "rubyist-aasm", :source => "http://gems.github.com", :lib => 'aasm'
 
 require 'rails_commerce'
