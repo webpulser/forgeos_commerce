d35218aa236ba1c24ffbd3a91044779e564f5686
clepagnot <clepagnot@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-08-31 16:32:11 +0000

    - fix pack duplication route - add url tattribute type instead of file & picture

--- config/desert_routes.rb
+++ config/desert_routes.rb
@@ -33,7 +33,7 @@ namespace :admin do |admin|
   admin.resources :pictures
   admin.resources :product_types
   admin.resources :products, :collection => { :url => :post }, :member => { :activate => :post, :update_tattributes_list => :post, :duplicate => :get }
-  admin.resources :packs, :controller => :products,  :collection => { :url => :post }, :member => { :activate => :post }
+  admin.resources :packs, :controller => :products,  :collection => { :url => :post }, :member => { :activate => :post, :duplicate => :get }
   admin.resources :tags
   admin.resources :special_offers, :collection => { :special_offer => [:get, :post] }
   admin.resources :shipping_rules
