6c0b6b3d4542f8ee997c8e76a5167fe9093629d0
sdeloor <sdeloor@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-09-01 13:50:43 +0000

    remove migration quantity

--- db/migrate/20090901082728_add_quantity_to_cart_products.rb
+++
@@ -1,9 +0,0 @@
-class AddQuantityToCartProducts < ActiveRecord::Migration
-  def self.up
-    add_column :carts_products, :quantity, :integer
-  end
-
-  def self.down
-    remove_column :carts_products, :quantity
-  end
-end
--- db/migrate/20090901114241_remove_quantity_from_carts_product.rb
+++
@@ -1,9 +0,0 @@
-class RemoveQuantityFromCartsProduct < ActiveRecord::Migration
-  def self.up
-    remove_column :carts_products, :quantity
-  end
-
-  def self.down
-    add_column :carts_products, :quantity, :integer
-  end
-end
