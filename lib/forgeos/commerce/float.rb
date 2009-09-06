82fb3ff0bdb0a1ea3f10588985292102f8ea5a6e
clepagnot <clepagnot@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-09-06 12:41:07 +0000

    - fix dashboard statistics

--- lib/forgeos/commerce/float.rb
+++ lib/forgeos/commerce/float.rb
@@ -2,4 +2,9 @@ class Float
   def decimal(precision = 2)
     ((self % 1) * 10.rpower(precision)).round
   end
+  
+  def diff(number)
+    return 100 if number.zero?
+    (((self - number) / number)* 100).round
+  end
 end
