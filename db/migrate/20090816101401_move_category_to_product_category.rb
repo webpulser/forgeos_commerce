099032976b45b657bdd8da60f09038477eadc0f2
fcogez <fcogez@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-09-16 14:43:39 +0000

    cleaning migration

--- db/migrate/20090816101401_move_category_to_product_category.rb
+++ db/migrate/20090816101401_move_category_to_product_category.rb
@@ -1,8 +1,5 @@
 class MoveCategoryToProductCategory < ActiveRecord::Migration
   def self.up
-    change_table :categories do |t|
-      t.string :type
-    end
     rename_table :categories_products, :product_categories_products
     change_table :product_categories_products do |t|
       t.rename :category_id, :product_category_id
@@ -16,8 +13,5 @@ class MoveCategoryToProductCategory < ActiveRecord::Migration
       t.timestamps
     end
     rename_table :product_categories_products, :categories_products
-    change_table :categories do |t|
-      t.remove :type
-    end
   end
 end
