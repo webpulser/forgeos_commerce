4de9828553e6213f3e65d5a0e4052865b616142f
cveneziani <cveneziani@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-09-29 14:33:17 +0000

    - removed useless controllers : order_details, pictures, tags - removed useless views : order_details, categories, pictures, tags, associated_elements - removed useless helpers

--- app/helpers/admin/pictures_helper.rb
+++
@@ -1,6 +0,0 @@
-module Admin::PicturesHelper
-  def new_admin_picture_path_with_session_information
-    session_key = ActionController::Base.session_options[:key]
-    admin_pictures_path(session_key => cookies[session_key], request_forgery_protection_token => form_authenticity_token)
-  end
-end
