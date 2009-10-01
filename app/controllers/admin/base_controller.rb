98b84837b86f6d309f5eaffc7107b3c7c94b59a8
clepagnot <clepagnot@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-10-01 08:33:24 +0000

    - skip set_currency in admin url generation

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,4 @@
 class Admin::BaseController < ApplicationController 
   skip_before_filter :get_cart, :get_wishlist
-  skip_before_filter :set_currency, :only => :notifications
+  skip_before_filter :set_currency, :only => [:notifications, :url]
 end
