fe61be025a422a511dcfbb5135987abf10673efe
fcogez <fcogez@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-10-02 13:33:14 +0000

    updating rails commerce init

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-I18n.load_path += Dir[Rails.root.join('vendor', 'plugins', 'rails_commerce', 'config', 'locales', '*.{rb,yml}')]
+I18n.load_path += Dir[File.join(RAILS_ROOT, 'vendor', 'plugins', 'rails_commerce', 'config', 'locales', '**', '*.{rb,yml}')]
 config.gem "rubyist-aasm", :source => "http://gems.github.com", :lib => 'aasm'
 
 require 'rails_commerce'
