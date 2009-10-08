ea3ef3e341a81a2fe0ad64cda368e07cb8c8b9af
cveneziani <cveneziani@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-10-08 13:38:37 +0000

    removed graph generation methods (moved to forgeos_core)

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -4,8 +4,6 @@ class Admin::StatisticsController < Admin::BaseController
   before_filter :customers_new, :only => :index
   before_filter :best_customers, :only => :index
 
-  before_filter :get_graph, :only => :index
-
   # generates the ofc2 graph
   def graph
     get_date
@@ -62,77 +60,5 @@ private
 
   def best_customers
       @best_buyers = Forgeos::Commerce::Statistics.best_customers(@date, 5)
-  end
-
-  def get_graph
-    @graph = open_flash_chart_object(666, 250, admin_statistics_graph_path(:timestamp => params[:timestamp]), true,'/flashes/')
-  end
-
-  def generate_graph(element, y_max, colour)
-    # Conf for X axis
-    steps = 4
-    days_step = (@date.count / steps) > 0 ? @date.count / steps : 1
-
-    x_labels = XAxisLabels.new
-    x_labels.set_steps(days_step)
-    case params[:timestamp]
-    when 'week'
-      x_labels.labels = @date.collect{|day| day.to_s(:short)}
-    else
-      x_labels.labels = @date.collect{|day| day.beginning_of_month == day ? day.to_s(:short) : day.to_s(:only_day)}
-      x_labels.set_steps(10)
-    end
-    x_labels.colour = '#7D5223'
-    
-    x_axis = XAxis.new
-    x_axis.colour = '#7D5223'
-    x_axis.grid_colour = '#C8A458'
-    x_axis.set_steps(days_step)
-    x_axis.set_stroke(1)
-    x_axis.set_tick_height(0)
-    x_axis.set_labels(x_labels)
-
-    # Conf for Y axis
-    y_axis = YAxis.new
-    y_axis.colour = '#F2B833'
-    y_axis.set_range(0, y_max[0], (y_max[0])/4) if y_max
-    y_axis.tick_length = 0
-    y_axis.stroke = 0
-
-    # Conf for Y right axis 
-    y_axis_right = YAxisRight.new
-    y_axis_right.set_range(0, y_max[1], (y_max[1])/4) if y_max
-    y_axis_right.tick_length = 0
-    y_axis_right.stroke = 0
-    y_axis_right.colour = '#94CC69'
-    y_axis_right.set_label_text("#val# #{$currency.html}")
-
-    # Tooltip
-    tooltip = Tooltip.new
-    tooltip.set_shadow(false)
-    tooltip.stroke = 1
-    tooltip.colour = '#000000'
-    tooltip.set_background_colour("#ffffff")
-    tooltip.set_title_style("{font-size: 10px; font-weight: normal; color: #000000;}")
-    tooltip.set_body_style("{font-size: 12px; font-weight: bold; color: #000000;}")
-
-    # Construct chart
-    chart = OpenFlashChart.new
-    chart.set_bg_colour('#FEF7DB')
-    chart.x_axis = x_axis
-    chart.y_axis = y_axis
-    chart.y_axis_right = y_axis_right
-    chart.set_tooltip(tooltip)
-    #chart.y_legend = { :text => 'toto' }
-    #chart.y_legend_right = { :text => 'tata' }
-    if element.is_a?(Array)
-      element.each do |data|
-        chart.add_element(data)
-      end
-    else
-      chart.add_element(element)
-    end
-
-    return chart.render
   end
 end
