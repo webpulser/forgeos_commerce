ff5ff5a94db71691a150ac12b59418ba0f527590
clepagnot <clepagnot@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-10-09 13:22:51 +0000

    - fix forgeos_commerce init

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 I18n.load_path += Dir[File.join(RAILS_ROOT, 'vendor', 'plugins', 'forgeos_commerce', 'config', 'locales', '**', '*.{rb,yml}')]
 config.gem "rubyist-aasm", :source => "http://gems.github.com", :lib => 'aasm'
 
-require 'forgeos_commerce'
+require 'forgeos/commerce'
