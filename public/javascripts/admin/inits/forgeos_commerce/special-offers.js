d4f3dfd141d8f47d90a46dc93250bb373a7d52ff
jclefrancois <jclefrancois@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-10-12 16:10:48 +0000

    update design for compatibility

--- public/javascripts/admin/inits/forgeos_commerce/special-offers.js
+++ public/javascripts/admin/inits/forgeos_commerce/special-offers.js
@@ -1,6 +1,12 @@
 jQuery(document).ready(function(){
   $('#date_option_preview').datepicker({showOn: 'both',buttonText: ''});
-  $('input.date-picker').datepicker({showOn: 'both',buttonText: ''});
+
+  $('input.date-picker').datepicker({
+    showOn: 'both',
+    buttonText: '',
+    changeMonth: true,
+    changeYear: true
+  });
 
   //init the special-Offer dataSlide
   $('#specialOffers_products_table').dataSlide({
