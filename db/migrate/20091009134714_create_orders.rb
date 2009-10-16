81e914db9609080c75e5f7b1b700fe155f7f4beb
clepagnot <clepagnot@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-10-16 09:41:55 +0000

    - revert to r679

--- db/migrate/20091009134714_create_orders.rb
+++ db/migrate/20091009134714_create_orders.rb
@@ -3,6 +3,7 @@ class CreateOrders < ActiveRecord::Migration
     create_table :orders do |t|
       t.belongs_to :user
       t.string :status,
+        :voucher,
         :transaction_number,
         :reference
       t.timestamps
