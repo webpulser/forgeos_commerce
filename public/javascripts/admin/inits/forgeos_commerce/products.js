420f4b1f0e8c326149c3a8c96dd9c6640177bbbf
cveneziani <cveneziani@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-10-23 08:16:07 +0000

    moved product javascript code from special offers to products js

--- public/javascripts/admin/inits/forgeos_commerce/products.js
+++ public/javascripts/admin/inits/forgeos_commerce/products.js
@@ -25,4 +25,18 @@ jQuery(document).ready(function(){
     $('#existing-products').toggleClass('open');
     $('#existing-products').toggle('blind');
   });
+
+  $('#product_product_type_id, #pack_product_type_id').live('change',function(e){
+      $.ajax({
+        before: function(){ tmce_unload_children('#product-types-panel');},
+        data: { product_type_id: $(e.target).val(), authenticity_token: AUTH_TOKEN, id: get_id_from_rails_url() },
+        success: function(request){
+          $('#product-types-panel').html(request);
+          InitCustomSelects();
+          tmce_load_children('#product-types-panel');
+        },
+        type:'post',
+        url:'/admin/products/update_attributes_list'
+      });
+  });
 });
--- public/javascripts/admin/inits/forgeos_commerce/special-offers.js
+++ public/javascripts/admin/inits/forgeos_commerce/special-offers.js
@@ -12,19 +12,4 @@ jQuery(document).ready(function(){
   $('#specialOffers_products_table').dataSlide({
     "sPaginationType": "full_numbers"
   });
-
-  $('#product_product_type_id, #pack_product_type_id').live('change',function(e){
-      $.ajax({
-        before: function(){ tmce_unload_children('#product-types-panel');},
-        data: { product_type_id: $(e.target).val(), authenticity_token: AUTH_TOKEN, id: get_id_from_rails_url() },
-        success: function(request){
-          $('#product-types-panel').html(request);
-          InitCustomSelects();
-          tmce_load_children('#product-types-panel');
-        },
-        type:'post',
-        url:'/admin/products/update_attributes_list'
-      });
-  });
-
 });
