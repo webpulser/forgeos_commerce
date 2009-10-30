0e85873f3b28a90fa8e8b20b8dcee42a1c32e945
cveneziani <cveneziani@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-10-30 15:23:41 +0000

    moved special_offers scripts from carousels js

--- public/javascripts/admin/inits/forgeos_commerce/special-offers.js
+++ public/javascripts/admin/inits/forgeos_commerce/special-offers.js
@@ -1,6 +1,14 @@
 jQuery(document).ready(function(){
   $('#date_option_preview').datepicker({showOn: 'both',buttonText: ''});
 
+  $('#limited-time-offer-end').bind('click', function() {
+    $(this).val('');
+  });
+
+  $('#limited-time-offer-end').bind('blur', function() {
+    $(this).val('dd/mm/yyyy');
+  });
+
   $('input.date-picker').datepicker({
     showOn: 'both',
     buttonText: '',
