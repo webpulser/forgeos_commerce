1fa19be4d49cad65f5982ac38d05b60db6e869df
clepagnot <clepagnot@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-11-18 08:46:48 +0000

    * remove rights dataTable functions from forgeos_commerce

--- public/javascripts/admin/functions/forgeos_commerce/orders.js
+++ public/javascripts/admin/functions/forgeos_commerce/orders.js
@@ -74,39 +74,3 @@ function update_order_total(){
     type:'post'
   });
 }
-
-/* Discard inline crud */
-function discard() {
-  $('#new_right').remove();
-  $('.create-right').parent().show();
-  $('#table').find('.edit-right').show();
-  $('#table').find('.duplicate-right').show();
-  $('#table').unwrap();
-}
-
-/* Disable all edit-right & duplicate-right & the create right button*/
-function disable_links() {
-  $('.create-right').parent().hide();
-  $('#table').find('.edit-right').hide();
-  $('#table').find('.duplicate-right').hide();
-}
-
-function form_ajax_right(url, method){
-  onsubmit_ajax = "$.ajax({\n\
-      success: function(result){\n\
-        $('#table').dataTableInstance().fnDraw(); \n\
-        $('#table').unwrap(); \n\
-        $('.create-right').parent().show();\n\
-      }, \n\
-      error: function(){\n\
-        display_notifications();\n\
-      }, \n\
-      data: $.param($(this).serializeArray()) + '&authenticity_token=' + encodeURIComponent('" + AUTH_TOKEN + "'), \n\
-      dataType: 'script', \n\
-      type: '"+method+"', \n\
-      url: '"+url+"'\n\
-    }); \n\
-    return false;"
-
-  return onsubmit_ajax;
-}
