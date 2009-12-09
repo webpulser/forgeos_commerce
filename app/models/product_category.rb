758dc80959c06b818dc6d8e1a36414a7ac1e1e97
clepagnot <clepagnot@825a9613-bfa8-4570-be30-ef0fa25aad10>
2009-12-09 09:17:21 +0000

    * fix sort for products

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,3 +1,3 @@
 class ProductCategory < Category
-  has_and_belongs_to_many :elements, :join_table => 'categories_elements', :foreign_key => 'category_id', :association_foreign_key => 'element_id', :class_name => 'Product', :order => :name
+  has_and_belongs_to_many :elements, :join_table => 'categories_elements', :foreign_key => 'category_id', :association_foreign_key => 'element_id', :class_name => 'Product'
 end
