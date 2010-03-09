f270a01d7a901fb7b0d8a3831f7a7c130f62493d
cyrill62 <cyril.lepagnot@webpulser.com>
2010-03-09 09:37:59 +0100

    use engines rather than desert

--- init.rb
+++ init.rb
@@ -1,8 +1,4 @@
 I18n.load_path += Dir[File.join(RAILS_ROOT, 'vendor', 'plugins', 'forgeos_commerce', 'config', 'locales', '**', '*.{rb,yml}')]
 config.gem 'aasm', :source => 'http://gemcutter.org', :version => '2.1.5'
 
-if RAILS_ENV == 'development'
-  ActiveSupport::Dependencies.load_once_paths.reject!{|x| x =~ /^#{Regexp.escape(File.dirname(__FILE__))}/}
-end
-
 require 'forgeos/commerce'
