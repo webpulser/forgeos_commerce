f7ce43f60f94aff20e5571a373c4e16b53802fc4
cyrill62 <cyril.lepagnot@webpulser.com>
2010-03-10 10:37:56 +0100

    improve files load

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-I18n.load_path += Dir[File.join(RAILS_ROOT, 'vendor', 'plugins', 'forgeos_commerce', 'config', 'locales', '**', '*.{rb,yml}')]
 config.gem 'aasm', :source => 'http://gemcutter.org', :version => '2.1.5'
 
-require 'forgeos/commerce'
+require File.join(File.dirname(__FILE__), 'lib','forgeos','menu')
+require File.join(File.dirname(__FILE__), 'lib','forgeos','commerce')
