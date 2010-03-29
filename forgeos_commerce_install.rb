7344bbb3c3b881854e3fb2e4bd53a9b94f030543
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2010-03-29 11:06:57 +0200

    update README & install template

--- forgeos_commerce_install.rb
+++ forgeos_commerce_install.rb
@@ -10,53 +10,14 @@ plugin 'forgeos_core', :git => 'git://github.com/webpulser/forgeos_core.git'
 plugin 'forgeos_cms', :git => 'git://github.com/webpulser/forgeos_cms.git'
 plugin 'forgeos_commerce', :git => 'git://github.com/webpulser/forgeos_commerce.git'
 
-file 'db/migrate/001_install_forgeos_core.rb', <<-END
-class InstallForgeosCore < ActiveRecord::Migration
-  def self.up
-    migrate_plugin :forgeos_core, 20091106163507
-  end
-                            
-  def self.down
-    migrate_plugin :forgeos_core, 0
-  end
-end
-END
-
-file 'db/migrate/002_install_forgeos_cms.rb', <<-END
-class InstallForgeosCms < ActiveRecord::Migration
-  def self.up
-    migrate_plugin :forgeos_cms, 20091009134736
-  end
-                            
-  def self.down
-    migrate_plugin :forgeos_cms, 0
-  end
-end                         
-END
-
-file 'db/migrate/003_install_forgeos_commerce.rb', <<-END
-class InstallForgeosCommerce < ActiveRecord::Migration
-  def self.up
-    migrate_plugin :forgeos_commerce, 20091009134947
-  end
-                            
-  def self.down
-    migrate_plugin :forgeos_commerce, 0
-  end
-end
-END
-
-
-environment "config.plugins = [ :forgeos_core, :forgeos_cms, :all ]"
+environment "config.plugins = [ :forgeos_core, :all ]"
 gem 'acts-as-taggable-on', :source => 'http://gemcutter.org'
-gem 'desert' 
-append_file 'Rakefile', "require 'desert'"
+append_file 'config/boot.rb', "require File.join(File.dirname(__FILE__), '..','vendor','plugins','engines','boot')"
 run 'cp vendor/plugins/forgeos_core/config/attachments.example.yml config/attachments.yml'
 
-route 'map.routes_from_plugin(:forgeos_cms)'
-route 'map.routes_from_plugin(:forgeos_core)'
-route 'map.routes_from_plugin(:forgeos_commerce)'
-route "map.root :controller => 'url_catcher', :action => 'page', :url => 'index'"
+route "map.connect ':controller/:action/:id'"
+route "map.connect ':controller/:action/:id.:format'"
+route "map.page '*url', :controller => 'url_catcher', :action => 'page'"
 
 rake 'db:create'
 rake 'gems:install', :sudo => true
