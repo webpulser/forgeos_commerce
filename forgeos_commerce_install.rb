d85f68d4365c86fc8f790bc0f11625e1620cae10
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2010-05-26 10:01:48 +0200

    use github repo in template

--- forgeos_commerce_install.rb
+++ forgeos_commerce_install.rb
@@ -1,13 +1,13 @@
 git :init
 plugin 'engines', :git => 'git://github.com/lazyatom/engines.git'
 append_file 'config/boot.rb', "require Rails.root.join('vendor','plugins','engines','boot')"
+
 plugin 'attachment_fu', :git => 'git://github.com/technoweenie/attachment_fu.git'
 plugin 'localized_dates', :git => 'git://github.com/clemens/localized_dates.git'
 plugin 'open_flash_chart', :git => 'git://github.com/pullmonkey/open_flash_chart.git'
-
-plugin 'forgeos_core', :git => '-b engines http://src.forgeos.com/forgeos/core', :submodule => true
-plugin 'forgeos_cms', :git => '-b engines http://src.forgeos.com/forgeos/cms', :submodule => true
-plugin 'forgeos_commerce', :git => '-b engines http://src.forgeos.com/forgeos/commerce', :submodule => true
+plugin 'forgeos_core', :git => '-b engines http://github.com/webpulser/forgeos_core.git', :submodule => true
+plugin 'forgeos_cms', :git => '-b engines http://github.com/webpulser/forgeos_cms.git', :submodule => true
+plugin 'forgeos_commerce', :git => '-b engines http://github.com/webpulser/forgeos_commerce.git', :submodule => true
 
 environment "config.plugins = [ :forgeos_core, :all ]"
 gem 'acts-as-taggable-on', :source => 'http://gemcutter.org'
