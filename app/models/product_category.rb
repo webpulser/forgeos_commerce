3e0914f119d269708feea5d19f403cd426ad84ea
Sébastien Dumont <sebastien.dumont@webpulser.com>
2010-06-22 19:17:22 +0200

    product categories: don't count soft deleted product

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,3 +1,10 @@
 class ProductCategory < Category
   has_and_belongs_to_many :elements, :join_table => 'categories_elements', :foreign_key => 'category_id', :association_foreign_key => 'element_id', :class_name => 'Product'
+
+  def total_elements_count_with_deleted
+    return 0 if self.elements.empty? & children.empty?
+    ([self.elements.find(:all, :conditions => { :deleted => false}).size] + children.collect(&:total_elements_count)).sum
+  end
+  alias_method_chain :total_elements_count, :deleted
+
 end
