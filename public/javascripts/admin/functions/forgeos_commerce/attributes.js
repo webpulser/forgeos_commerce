dca703cd8d46ec768cfa1894c7e861800adadb99
Sébastien Dumont <sebastien.dumont@webpulser.com>
2010-06-23 18:14:38 +0200

    attributes: rename _delete into _destroy

--- public/javascripts/admin/functions/forgeos_commerce/attributes.js
+++ public/javascripts/admin/functions/forgeos_commerce/attributes.js
@@ -3,7 +3,7 @@ function add_item_on_add_click() {
   var new_choice = '<li class="block-container">';
   new_choice += '<span class="block-type"> <span class="handler"> <span class="inner" /> </span> </span> </span>';
   new_choice += '<span class="block-name"> <input type="text" id="attribute_values_attributes_'+false_id+'" size="30" name ="'+object_name+'[attribute_values_attributes]['+false_id+'][name]" /> ';
-  new_choice += '<input type="hidden" class="delete" name="'+object_name+'[attribute_values_attributes]['+false_id+'][_delete]" />';
+  new_choice += '<input type="hidden" class="delete" name="'+object_name+'[attribute_values_attributes]['+false_id+'][_destroy]" />';
   new_choice += '<input type="hidden" name="'+object_name+'[attribute_values_attributes]['+false_id+'][position]" value=""/>';
   new_choice += '</span> <span class="small-icons red-delete-icon" />';
   new_choice += '</span> <span class="small-icons green-add-icon" />';
