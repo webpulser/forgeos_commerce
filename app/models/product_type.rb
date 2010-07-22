9267071b663d4674ef8285712a4d8366f54904a8
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2010-07-22 12:22:38 +0200

    fix product_type search index

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,18 +1,25 @@
 class ProductType < ActiveRecord::Base
   translates :name, :url, :description
+  define_translated_index :name, :url, :description
+
   has_many :products, :dependent => :destroy
   has_and_belongs_to_many_attachments
+
   has_and_belongs_to_many :product_attributes, :class_name => 'Attribute', :readonly => true
-  has_and_belongs_to_many :dynamic_attributes, :class_name => 'Attribute', :readonly => true, :join_table => 'attributes_product_types', :association_foreign_key => 'attribute_id',
+  has_and_belongs_to_many :dynamic_attributes, :class_name => 'Attribute', :readonly => true,
+    :join_table => 'attributes_product_types', :association_foreign_key => 'attribute_id',
     :conditions => {:dynamic => true}
 
-  has_and_belongs_to_many :product_type_categories, :readonly => true, :join_table => 'categories_elements', :foreign_key => 'element_id', :association_foreign_key => 'category_id'
+  has_and_belongs_to_many :product_type_categories, :readonly => true,
+    :join_table => 'categories_elements', :foreign_key => 'element_id',
+    :association_foreign_key => 'category_id'
+
+  has_and_belongs_to_many :categories, :readonly => true,
+    :join_table => 'categories_elements', :foreign_key => 'element_id',
+    :association_foreign_key => 'category_id', :class_name => 'ProductTypeCategory'
 
   validates_presence_of :name
 
-  define_index do
-    indexes name, :sortable => true
-  end
 
   # Set all Products dynamic_attributes on save
   def before_save
