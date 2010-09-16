7ec8cf534224982b32fada49219196394f23f0ed
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2010-09-16 16:48:27 +0200

    lightens init.rb

--- init.rb
+++ init.rb
@@ -1,5 +1,4 @@
 config.gem 'aasm', :version => '>=2.1.5'
 
-require File.join(File.dirname(__FILE__), 'lib','forgeos','menu')
-require File.join(File.dirname(__FILE__), 'lib','forgeos','commerce')
+require 'forgeos_commerce'
 puts 'Forgeos Commerce loaded'
--- lib/forgeos/commerce.rb
+++
@@ -1,7 +0,0 @@
-module Forgeos
-  module Commerce
-  end
-end
-
-require 'forgeos/commerce/float'
-#TODO REMOVE $currency = Currency.find_by_code('EUR') if Currency.table_exists?
