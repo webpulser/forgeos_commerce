e3db8d4b20834042a000c2e1553250572781e868
Sébastien Dumont <sebastien.dumont@webpulser.com>
2011-02-22 14:56:41 +0100

    user: fix addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ require File.join(Rails.plugins['forgeos_core'].directory,'app','models','user')
 class User < Person
   has_one :cart, :dependent => :destroy
   has_one :wishlist, :dependent => :destroy
-  has_one :address_delivery, :order => 'id desc'
-  has_one :address_invoice, :order => 'id desc'
+  has_one :address_delivery, :foreign_key => :person_id, :order => 'id desc'
+  has_one :address_invoice, :foreign_key => :person_id, :order => 'id desc'
 
-  has_many :address_deliveries, :order =>  'id desc', :dependent => :destroy
-  has_many :address_invoices, :order => 'id desc', :dependent => :destroy
+  has_many :address_deliveries, :foreign_key => :person_id, :order =>  'id desc', :dependent => :destroy
+  has_many :address_invoices, :foreign_key => :person_id, :order => 'id desc', :dependent => :destroy
   has_many :orders, :order => 'id desc'
 
   accepts_nested_attributes_for :address_deliveries, :address_delivery, :address_invoices, :address_invoice
