78272c78872225a03a85b29635be0b7703a979f3
Sébastien Dumont <sebastien.dumont@webpulser.com>
2011-03-03 16:34:08 +0100

    remove front menu

--- lib/forgeos/menu.rb
+++ lib/forgeos/menu.rb
@@ -25,20 +25,4 @@ module Forgeos
     :url => { :controller => 'admin/special_offers'},
     :html => { :class => 'right' }
   }
-
-  # Set site's menu
-  Menu << { :title => 'home', :url => :root, :i18n => true }
-  Menu << { :title => 'catalog', :url => { :controller => 'catalog' }, :i18n => true }
-  Menu << { :title => ['cart', { :count => 1}], 
-    :html => { :id => 'forgeos_commerce_cart_link' },
-    :url => { :controller => 'cart' },
-    :helper => { :method => 'link_to_cart' },
-    :i18n => true
-  } 
-  Menu << { :title => ['wishlist',{ :count => 1}],
-    :html => { :id => 'forgeos_commerce_wishlist_link' },
-    :url => { :controller => 'wishlist' },
-    :helper => { :method => 'link_to_wishlist' },
-    :i18n => true 
-  }
 end
