62e7679f4e94dffbecaec2a4f417a781b7d7ebd1
Sébastien <seb@mfzb.com>
2011-03-10 11:58:39 +0100

    add current cart to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_currency
+  helper_method :current_currency, :current_cart
+  
   # Change the currency
   def change_currency(currency_id)
     if currency = Currency.find_by_id(currency_id)
@@ -20,4 +21,20 @@ private
       end
     end
   end
+  
+  def current_cart
+    return @cart if @cart
+    session[:cart_id] = current_user.cart.id if session[:cart_id].nil? && logged_in? && !current_user.is_a?(Administrator) && current_user.cart
+    @cart = Cart.find_by_id(session[:cart_id])
+    # If current_cart is nil because a problem of session or db.
+    # This recursive call method, risk of stack error if this problem persist.
+    if @cart.nil?
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+    # Associate cart with user if he's logged
+    @cart.update_attribute(:user_id, current_user.id) if logged_in?
+    return @cart
+  end
+  
 end
