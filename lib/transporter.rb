63cd660acf870092400d3a2069630079a504710f
Jean-Charles Lefrancois <jcl@webpulser.com>
2011-04-11 10:57:55 +0200

    fix spelling

--- lib/transporter.rb
+++ lib/transporter.rb
@@ -1,13 +1,14 @@
 require 'ruleby'
 class Transporter < Ruleby::Rulebook
 
-  attr_writer :transporter_ids, :orderi, :cart
+  attr_writer :transporter_ids, :order, :cart
   
   def rules
     TransporterRule.find_all_by_active(true).each do |transporter|
       condition = transporter.conditions
       condition.gsub!(/Cart/,'Order') unless @cart
       rule eval(condition) do |context|
+
         if @cart && @cart.address_delivery && !transporter.geo_zones.empty? && transporter.geo_zones.map(&:id).include?(@cart.address_delivery.country.id)
           @transporter_ids << transporter.id
         elsif @cart && @cart.address_delivery && transporter.geo_zones.empty?
