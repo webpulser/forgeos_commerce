67e43bc0b9dcc0b6642511c062c162c05b3ea066
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-05-26 16:08:55 +0200

    optimize product_category total_elements_count

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -2,8 +2,7 @@ class ProductCategory < Category
   has_and_belongs_to_many :elements, :join_table => 'categories_elements', :foreign_key => 'category_id', :association_foreign_key => 'element_id', :class_name => 'Product', :list => true
 
   def total_elements_count_with_deleted
-    return 0 if self.elements.empty? & children.empty?
-    ([self.elements.find(:all, :conditions => { :deleted => false}).size] + children.collect(&:total_elements_count)).sum
+    ([self.elements.count('id',:conditions => { :deleted => false})] + children.all(:select => 'id,type').map(&:total_elements_count)).sum
   end
   alias_method_chain :total_elements_count, :deleted
 
