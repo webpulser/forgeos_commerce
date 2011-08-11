66bc76cfdaa46d85f3fffb04226238a74a967e25
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-08-11 11:40:27 +0200

    upgrade routes to rails 3 routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,98 @@
-ActionController::Routing::Routes.draw do |map|
+Forgeos::Commerce::Engine.routes.draw do
 
-  map.namespace :admin do |admin|
-    admin.resources :transporters, :controller => 'transporter_rules', :member => { :activate => :post, :duplicate => :get }
-    admin.resources :transporter_rules, :member => { :duplicate => :get }
-    admin.resources :vouchers, :member => { :activate => :post }
-    admin.resources :brands
+  namespace :admin do
+
+    match '/admin/get_cross_selling_id' => 'admin/products#get_cross_selling_id'
+    match 'admin/transporter_categories.:format' => 'admin/transporter_rules#categories'
+
+    resources :transporters, :controller => 'transporter_rules' do
+      member do
+        post :activate
+        get :duplicate
+      end
+    end
+
+    resources :transporter_rules do
+      member do
+        get :duplicate
+      end
+    end
+
+    resources :vouchers do
+      member do
+        post :activate
+      end
+    end
+
+    resources :brands
+
     %w(brand product product_type attribute special_offer).each do |category|
-      admin.resources "#{category}_categories", :controller => 'categories', :requirements => { :type => "#{category}_category" }
+      resources "#{category}_categories", :controller => 'categories', :type => "#{category}_category"
     end
-    map.connect "admin/transporter_categories.json", :controller => 'admin/transporter_rules', :action => 'categories'
-    admin.resources :geo_zones, :member => { :add_element => :post }
-    admin.resources :countries, :controller => 'geo_zones'
 
-    admin.resources :attributes, :collection => { :access_method => :post }, :member => { :duplicate => :get}
-    admin.resources :dynamic_attributes, :collection => { :access_method => :post }, :member => { :duplicate => :get}, :controller => 'attributes'
+
+    resources :geo_zones do
+      member do
+        post :add_element
+      end
+    end
+
+    resources :countries, :controller => 'geo_zones'
+
+    %w(attributes dynamic_attributes).each do |model|
+      resources model, :controller => :attributes do
+        collection do
+          post :access_method
+        end
+        member do
+          get :duplicate
+        end
+      end
+    end
+
     %w(checkbox radiobutton picklist text longtext number date url).each do |attribute_type|
-      admin.resources "#{attribute_type}_attributes", :controller => 'attributes', :requirements => { :type => attribute_type }
+      resources "#{attribute_type}_attributes", :controller => 'attributes', :type => attribute_type
     end
 
-    admin.resources :orders, :member => { :bill => :get, :total => :put } do |order|
-      order.resources :details, :controller => 'order_details'
+    resources :orders do
+      member do
+        get :bill
+        put :total
+      end
+      resources :details, :controller => 'order_details'
     end
 
-    map.connect '/admin/get_cross_selling_id', :controller => 'admin/products', :action => 'get_cross_selling_id'
 
-    admin.resources :product_types
-    admin.resources :products, :collection => { :url => :post }, :member => { :activate => :post, :update_attributes_list => :post, :duplicate => :get }
-    admin.resources :packs, :collection => { :url => :post }, :member => { :activate => :post, :update_attributes_list => :post, :duplicate => :get }, :controller => :products, :requirements => { :type => 'pack' }
-    #admin.resources :special_offers, :collection => { :special_offer => [:get, :post] }
-    admin.resources :special_offers, :member => { :activate => :post }
-    admin.resources :forms
+    resources :product_types
+
+    resources :products do
+      collection do
+        post :url
+      end
+      member do
+        post :activate
+        post :update_attributes_list
+        get :duplicate
+      end
+    end
+
+    resources :packs, :controller => :products, :type => 'pack' do
+      collection do
+        post :url
+      end
+      member do
+        post :activate
+        post :update_attributes_list
+        get :duplicate
+      end
+    end
+
+    resources :special_offers do
+      member do
+        post :activate
+      end
+    end
+
+    resources :forms
   end
-
 end
