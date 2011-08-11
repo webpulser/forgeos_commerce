40e747f87ece758938c92967a9fce7fc5e490b43
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-08-11 11:38:12 +0200

    fix installation (tasks + template)

--- forgeos_commerce_install.rb
+++ forgeos_commerce_install.rb
@@ -1,25 +1,51 @@
 git :init
-plugin 'engines', :git => 'git://github.com/lazyatom/engines.git'
-append_file 'config/boot.rb', "require Rails.root.join('vendor','plugins','engines','boot')"
+
+gem 'rmagick'
+gem 'globalize3', '>= 0.1.0'
+gem 'acts-as-taggable-on', '>= 2.0.6'
+gem 'acts_as_list', '>= 0.1.3'
+gem 'acts_as_tree', '>= 0.1.1'
+gem 'bcrypt-ruby', '>= 2.1.4'
+gem 'fastercsv', '>= 1.5.4'
+gem 'haml', '>= 3.1.2'
+gem 'sass', '>= 3.1.4'
+gem 'mime-types', '>= 1.16'
+gem 'thinking-sphinx', '>= 2.0.5'
+gem 'webpulser-habtm_list', '>= 0.1.2'
+gem 'will_paginate', '~> 3.0.pre4'
+gem 'authlogic', '>= 3.0.3'
+gem 'forgeos_core', '>= 1.9.4'
+gem 'acts_as_commentable', '>= 3.0.1'
+gem 'forgeos_cms', '>= 1.9.4'
+gem 'aasm', '>= 2.2.0'
+gem 'ruleby', '>= 0.6'
+gem 'forgeos_commerce', '>= 1.9.0'
+
+say_status :warning, "If you get some errors from bundle (like missing gems) :", :red
+say_status :warning, "  * run bundle install", :red
+say_status :warning, "  * then rerun this template", :red
 
 plugin 'attachment_fu', :git => 'git://github.com/technoweenie/attachment_fu.git'
-plugin 'localized_dates', :git => 'git://github.com/clemens/localized_dates.git'
+gsub_file 'vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu.rb', 'RAILS_ROOT', 'Rails.root'
+
 plugin 'open_flash_chart', :git => 'git://github.com/pullmonkey/open_flash_chart.git'
-plugin 'forgeos_core', :git => '-b engines src.forgeos.com:forgeos/core', :submodule => true
-plugin 'forgeos_cms', :git => '-b engines src.forgeos.com:forgeos/cms', :submodule => true
-plugin 'forgeos_commerce', :git => '-b engines src.forgeos.com:forgeos/commerce', :submodule => true
 
-environment "config.plugins = [ :forgeos_core, :all]"
-gem 'acts-as-taggable-on'
 
-route "map.page '/*url', :controller => 'pages', :action => 'show'"
-route "map.connect ':controller/:action/:id.:format'"
-route "map.connect ':controller/:action/:id'"
-route "map.root :controller => 'pages', :action => 'index'"
+route "mount Forgeos::Cms::Engine => '/', :as => 'forgeos_cms'"
+route "mount Forgeos::Commerce::Engine => '/', :as => 'forgeos_commerce'"
+route "mount Forgeos::Core::Engine => '/', :as => 'forgeos_core'"
 
 rake 'db:create'
-run './script/generate plugin_migration'
+rake 'forgeos_core_engine:install:migrations'
+rake 'forgeos_cms_engine:install:migrations'
+rake 'forgeos_commerce_engine:install:migrations'
 rake 'db:migrate'
+rake 'forgeos:core:install'
+rake 'forgeos:cms:install'
 rake 'forgeos:commerce:install'
 
 run 'rm -rf config/locales/* public/*.html'
+
+say_status :warning, "If you get some errors from bundle (like missing gems) :", :red
+say_status :warning, "  * run bundle install", :red
+say_status :warning, "  * then rerun this template", :red
--- lib/tasks/install.rake
+++ lib/tasks/install.rake
@@ -1,14 +1,9 @@
 namespace :forgeos do
   namespace :commerce do
-    task :sync => ['forgeos:cms:sync'] do
-      system "rsync -r#{'v' unless Rails.env == 'production'}C #{File.join(Rails.plugins['forgeos_commerce'].directory,'public')} ."
+    desc "load fixtures and generate forgeos_commerce controllers ACLs"
+    task :install do
+      Rake::Task['forgeos:core:fixtures:load'].invoke('forgeos_commerce','currencies_exchanges_rates currencies pages page_translations people product_types product_type_translations')
+      Rake::Task['forgeos:core:generate:acl'].invoke(Gem.loaded_specs['forgeos_commerce'].full_gem_path)
     end
-
-    task :initialize => ['forgeos:cms:initialize'] do
-      system 'rake "forgeos:core:fixtures:load[forgeos_commerce,currencies_exchanges_rates currencies pages page_translations people product_types product_type_translations]"'
-      system "rake 'forgeos:core:generate:acl[#{Rails.plugins['forgeos_commerce'].directory}]'"
-    end
-
-    task :install => ['forgeos:cms:install', :initialize, :sync]
   end
 end
